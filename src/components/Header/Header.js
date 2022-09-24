fca6b5b0c307ac4de5fa67c5800df65ad111c62e
sa-sabuj <sasabuj321@gmail.com>
2022-09-24 23:21:10 +0600

    header & sidebar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import logo from '../../images/logo.png'
 import accountDetail from '../../images/account-detail.png'
-import './Header.css'
+import './Header.css';
+import userImg from '../../images/logo.png'
 
 const Header = () => {
     return (
@@ -15,8 +16,15 @@ const Header = () => {
                 <a href="/user">User Policy</a>
                 <a href="/contact">Contact Us</a>
             </div>
-            <div>
-                <img src={accountDetail} alt="" />
+            <div className='nav-right row'>
+                <span className='col-lg-2'><i class="fa-solid fa-bell notification"></i></span>
+                <div className="user-ing col-lg-2">
+                    <img src={userImg} alt="" />
+                </div>
+                <div className="mail-area col-lg-8">
+                    <p>sasabuj321@gmail.com</p>
+                    <small>Shamsul Alam Sabuj</small>
+                </div>
             </div>
 
         </nav>
--- src/components/MainSite/MainSite.js
+++ src/components/MainSite/MainSite.js
@@ -9,24 +9,28 @@ const MainSite = () => {
         <div>
             <div className='main-body '>
                 <div className="row">
-                    <div className="left-side col-lg-3">
+                    <div className=" left-side col-lg-3">
                         <ul>
                             <li>
+                                <span>
+                                    <i className="fa-sharp fa-solid fa-grip table-calls"></i>
+                                </span>
                                 <a href="">Dashboard</a>
                             </li>
-                            <li><a href="">Blogs</a></li>
-                            <li><a href="">Pages</a></li>
-                            <li><a href="">Site Setting</a></li>
-                            <li><a href="">Admin Roles</a></li>
-                            <li><a href="">Events</a></li>
-                            <li><a href="">Media</a></li>
+                            <li>
+                                <span><i className="fa-sharp fa-solid fa-message table-calls"></i></span><a href="">Blogs</a></li>
+                            <li><span><i className="fa-solid fa-file table-calls"></i></span><a href="">Pages</a></li>
+                            <li><span><i className="fa-solid fa-laptop table-calls"></i></span><a href="">Site Setting</a></li>
+                            <li><span><i className="fa-sharp fa-solid fa-person-shelter table-calls"></i></span><a href="">Admin Roles</a></li>
+                            <li><span><i className="fa-solid fa-bell table-calls"></i></span><a href="">Events</a></li>
+                            <li><span><i className="fa-solid fa-film table-calls"></i></span><a href="">Media</a></li>
                         </ul>
                         <div className="promo">
                             <img src={promo1} alt="" />
                         </div>
 
                         <div className="log-out">
-                       <button className='btn btn-center'>Sign Out</button>
+                            <button className='btn btn-center'>Sign Out</button>
                         </div>
                     </div>
                     <div className="middle-part col-lg-6"></div>
